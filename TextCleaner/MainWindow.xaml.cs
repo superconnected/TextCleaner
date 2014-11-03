c0d2a1d89aebf3b64a669cb2a62e9918a3f84e52
Josh Daniels <jdaniels@pcma.org>
2014-11-03 17:10:13 -0600

    initial commit

---
+++ TextCleaner/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextCleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string _acceptable = "<>/\\|;=\r\n\"' ()";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var input = tbText.Text;
+            if (String.IsNullOrEmpty(input))
+                return;
+
+            var output = new String(input.Where(c => Char.IsLetterOrDigit(c) || Char.IsPunctuation(c) || _acceptable.Contains(c)).ToArray());
+            tbText.Text = output;
+
+            MessageBox.Show("Cleaned!");
+        }
+    }
+}
